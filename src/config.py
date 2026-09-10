d3475c4301b7f2326c4378b729ba855a9cdf3f62
AH-Kharian <aihubkharian@gmail.com>
2026-09-10 08:16:25 -0700

    Added custom domain

--- src/config.py
+++ src/config.py
@@ -6,4 +6,4 @@ load_dotenv()
 class Config:
     FIREBASE_ACCOUNT_KEY=json.loads(os.getenv('FIREBASE_ACCOUNT_KEY'))
     RESEND_API_KEY=os.getenv('RESEND_API_KEY')
-    VERIFICATION_BASE_URL = "http://10.154.0.11:8000"
+    VERIFICATION_BASE_URL = "https://www.auth.aihubkharian.com"

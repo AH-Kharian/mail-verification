22ebb8c13c1c406921694803e4dde7791eda0216
AH-Kharian <aihubkharian@gmail.com>
2026-09-10 08:22:30 -0700

    Update VERIFICATION_BASE_URL in config.py

--- src/config.py
+++ src/config.py
@@ -6,4 +6,4 @@ load_dotenv()
 class Config:
     FIREBASE_ACCOUNT_KEY=json.loads(os.getenv('FIREBASE_ACCOUNT_KEY'))
     RESEND_API_KEY=os.getenv('RESEND_API_KEY')
-    VERIFICATION_BASE_URL = "https://www.auth.aihubkharian.com"
+    VERIFICATION_BASE_URL = "https://auth.aihubkharian.com"
